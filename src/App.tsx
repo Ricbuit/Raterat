4322a0985f9ecbed40f56033d8d95ad952bc09c6
r.buitenhuis <r.buitenhuis@conterra.de>
2025-06-02 10:32:42 +0200

    Initial setup with react ts + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <div className="bg-red-500 text-white p-4 rounded-xl">
+            Tailwind is working!
+        </div>
+    </>
+  )
+}
+
+export default App
